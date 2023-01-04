6c3cc2e21c9096a0df04d5bf7f9fd7a94e5d789f
Arthur Coelho Russo <arthur.c.russo.br@gmail.com>
2023-01-04 13:54:48 -0300

    Add files via upload

---
+++ type-string.js
@@ -0,0 +1,28 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá,mundo';
+const senha = "senhaSuperSegura456!" 
+const StringDeNumeros = "34567"
+
+const citacao = 'O leo disse "oi"';
+console.log(citacao)
+const citacao2 = "O leo disse 'oi'";
+console.log(citacao2)
+
+// concatenação (+)
+const meuNome = "Arthur"
+const frase = "Meu nome é ";
+
+console.log(frase + meuNome)
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+// template string OU template literal
+
